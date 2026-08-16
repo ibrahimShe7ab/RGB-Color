d9324b677f3bf8577bcefdb74ebb7ebe54210915
shehab-eldin-dev <ibrahimshehab2034@gmail.com>
2026-08-16 13:23:50 +0300

    finishing RGB COLOR APP

---
+++ app/src/main/java/com/example/rgbcolor/GreenActivity.kt
@@ -0,0 +1,31 @@
+package com.example.rgbcolor
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
+
+class GreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_green)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btn: MaterialButton = findViewById(R.id.toBlue_btn)
+        btn.setOnClickListener {
+            val i = Intent(this, BlueActivity::class.java)
+            startActivity(i)
+            finish()
+
+        }
+
+    }
+}
